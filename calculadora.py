1edc2b7b542312370d4e5e0f12c1f4a0ea3cb235
Ladislau <ladislaumartins@gmail.com>
2026-08-03 19:41:49 -0400

    feat: Funcionalidade da raiz quadrada

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,26 @@
+import math
 def subtracao(a,b):
     return a-b
 
 def soma(a,b):
     return a+b
-  
+
+def raiz_quadrada(numero):
+    if numero < 0:
+        return "Não existe raiz quadrada real de número negativo."
+    else:
+        return math.sqrt(numero)
+
+
+# Entrada de dados
+numero = float(input("Digite um número: "))
+
+# Chamada da função
+resultado = raiz_quadrada(numero)
+
+# Saída
+print("A raiz quadrada é:", resultado)
+
 def media_aritmetica(a, b):
     return (a + b) / 2
 
