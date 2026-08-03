8cf027cbcdf8573d3971b62e4c5da82fd3c99fe4
Marcelo <Marcelomfo1972@gmail.com>
2026-08-03 19:30:06 -0400

    separação da funcao resto de divisao da porcentagem

--- calculadora.py
+++ calculadora.py
@@ -1,13 +1,3 @@
-def calcular_porcentagem():
-    try:
-        valor = float(input("Digite o valor base: "))
-        porcentagem = float(input("Digite a porcentagem que deseja calcular: "))
-
-        resultado = (valor * porcentagem) / 100
-        print(f"➔ {porcentagem}% de {valor} é: {resultado}\n")
-    except ValueError:
-        print("Erro: Por favor, digite apenas números.\n")
-
 
 def resto_divisao():
     try:
