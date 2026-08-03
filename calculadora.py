3f1603b6e9d4341e93b0da87dc7456e75304d172
Marcelo <Marcelomfo1972@gmail.com>
2026-08-03 19:44:45 -0400

    funcao porcentagem

--- calculadora.py
+++ calculadora.py
@@ -8,17 +8,3 @@ def calcular_porcentagem():
     except ValueError:
         print("Erro: Por favor, digite apenas números.\n")
 
-
-def resto_divisao():
-    try:
-        dividendo = int(input("Digite o dividendo (número que será dividido): "))
-        divisor = int(input("Digite o divisor (número que vai dividir): "))
-
-        if divisor == 0:
-            print("Erro: Não é possível dividir por zero.\n")
-            return
-
-        resultado = dividendo % divisor
-        print(f"➔ O resto da divisão de {dividendo} por {divisor} é: {resultado}\n")
-    except ValueError:
-        print("Erro: Por favor, digite apenas números inteiros.\n")
