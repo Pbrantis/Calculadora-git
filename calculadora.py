31bd4781c60af274af6977c05efe8a6ffd1db876
João Vitor <joaodalimass@gmail.com>
2026-08-03 20:28:09 -0300

    feat: logaritmo

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,10 @@
-def subtracao(a,b):
-    return a-b
+import math
+
+numero = float(input("Digite um número: "))
+base = float(input("Digite a base do logaritmo: "))
+
+if numero > 0 and base > 0 and base != 1:
+    resultado = math.log(numero, base)
+    print("Resultado:", resultado)
+else:
+    print("Número ou base inválidos.")
