cfade3b8b47c0c84d4051d9f213627b9b57193d8
Ladislau <ladislaumartins@gmail.com>
2026-08-03 20:46:08 -0400

    feat: Funcionalidade valor absoluto

--- calculadora.py
+++ calculadora.py
@@ -1,46 +1,12 @@
 import math
-def subtracao(a,b):
-    return a-b
-
-def soma(a,b):
-    return a+b
-
-def raiz_quadrada(numero):
+def valor_absoluto(numero):
     if numero < 0:
-        return "Não existe raiz quadrada real de número negativo."
+        return -numero
     else:
-        return math.sqrt(numero)
+        return numero
 
-
-# Entrada de dados
 numero = float(input("Digite um número: "))
 
-# Chamada da função
-resultado = raiz_quadrada(numero)
+resultado = valor_absoluto(numero)
 
-# Saída
-print("A raiz quadrada é:", resultado)
-
-def media_aritmetica(a, b):
-    return (a + b) / 2
-
-def dividir(a, b):
-    if b == 0:
-        raise ValueError("Não é possível dividir por zero!")
-    return a / b
-
-def calcular_fatorial(n):
-    if n == 0 or n == 1:
-        return 1
-    resultado = 1
-    for i in range(2, n + 1):
-        resultado *= i
-    return resultado
-
-def calcular_tangente_triangulo(cateto_oposto, cateto_adjacente):
-    if cateto_adjacente == 0:
-        return "Erro: O cateto adjacente não pode ser zero."
-
-    tangente = cateto_oposto / cateto_adjacente
-    return tangente
-
+print("O valor absoluto é:", resultado)
